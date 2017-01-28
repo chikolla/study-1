233e63a0a121c6ae0abbdc5fa20cca8c094f1072
Kane Linch <kanagat.nugusbaev@mail.ru>
2017-01-28 17:56:33 +0600

    ended chapter Strings

---
+++ JavaEkkelTIJAndAlgorithms4/src/ekkel/strings/ReplacingStringTokenizer.java
@@ -0,0 +1,22 @@
+package ekkel.strings;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ * Created by halfmediator on 28.01.2017.
+ */
+public class ReplacingStringTokenizer {
+    public static void main(String[] args) {
+        String input = "But I’m not dead yet! I feel happy!";
+        StringTokenizer stoke = new StringTokenizer(input);
+        while(stoke.hasMoreElements())
+            System.out.print(stoke.nextToken() + " ^ ");
+        System.out.println();
+        System.out.println(Arrays.toString(input.split(" ")));
+        Scanner scanner = new Scanner(input);
+        while(scanner.hasNext())
+            System.out.print(scanner.next() + " ^ ");
+    }
+}
