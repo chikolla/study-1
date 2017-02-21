4b36ae106d78af8137453b311c0daf0f20f2f244
nugusbayevkk <kanagat.nugusbaev@mail.ru>
2017-02-21 21:41:54 +0600

    read ekkel collections chapter about collections functionality. and enterence in Sets

---
+++ JavaEkkelTIJAndAlgorithms4/src/ekkel/containers/Test.java
@@ -0,0 +1,13 @@
+package ekkel.containers;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by nugusbayevkk on 2/21/17.
+ */
+public class Test {
+    public static void main(String[] args){
+        Collection t = new ArrayList();
+    }
+}
